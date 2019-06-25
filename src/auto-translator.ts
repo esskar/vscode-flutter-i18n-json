9bba530d131d5d3c3c9f74296761c3c2483d284a
Sascha Kiefer <esskar@gmail.com>
2019-06-25 22:26:38 +0200

    use google-translate package for google translations

--- src/auto-translator.ts
+++ src/auto-translator.ts
@@ -1,37 +1,62 @@
 import { I18nConfig } from "./i18n.interfaces";
+import { Variables } from "./variables";
+
+interface GoogleError {
+  error?: GoogleError;
+  code?: number;
+  message?: string;
+  errors?: GoogleError[];
+  status?: string;
+}
+
+interface GoogleTranslation {
+  originalText: string;
+  translatedText: string;
+}
 
 // Translates given strings to specified locale,
 // using global I18nConfig having API key and defaultLocale
 export class AutoTranslator {
 
+  private readonly varibales = new Variables();
+
   constructor(private config: I18nConfig) {
   }
 
-  async translate(input: String, locale: string): Promise<String> {
-    return "!!!" + input;
-    /*
-    const key = this.config.googleTranslateApiKey;
-    if (!key) {
+  async translate(input: String, locale: string): Promise<string> {
+    const apiKey = this.config.googleTranslateApiKey;
+    if (!apiKey) {
       throw "googleTranslateApiKey is not set.";
     }
 
-    const query = {
-      q: input,
-      source: this.config.defaultLocale.substr(0, 2),
-      target: locale.substr(0, 2)
-    };
+    const source = this.config.defaultLocale.substr(0, 2);
+    const target = locale.substr(0, 2);
 
-    const options = {
-      uri: `https://www.googleapis.com/language/translate/v2?key=${key}`,
-      method: "POST",
-      qs: query,
-      headers: {
-        "User-Agent": "Request-Promise"
-      },
-      json: true // Automatically parses the JSON string in the response
-    };
+    const googleTranslate = require('google-translate')(apiKey);
+    return new Promise<string>((resolve, reject) => {
+      googleTranslate.translate(input, source, target, (error: any, translation: GoogleTranslation) => {
+        if (error) {
+          if (error.body) {
+            const ge = JSON.parse(error.body) as GoogleError;
+            const message = ge.message || ge.error && ge.error.message;
+            reject(message);
+          } else {
+            reject(error);
+          }
+        } else {
+          const result = this.handleTranslation(translation);
+          resolve(result);
+        }
+      });
+    });
+  }
 
-    const result = await request(options);
-    return result.data.translations[0].translatedText;*/
+  private handleTranslation(translation: GoogleTranslation): string {
+    const originalVars = this.varibales.parseVariables(translation.originalText) || [];
+    const translatedVars = this.varibales.parseVariables(translation.translatedText) || [];
+
+    const result = this.varibales.replaceVariablesWithVariables(
+      translation.translatedText, translatedVars, originalVars);
+    return result;
   }
 }
